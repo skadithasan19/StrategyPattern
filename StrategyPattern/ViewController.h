4c502d521938f19b109d40429fbd09003a18b0f4
Md Adit Hasan <skadithasan19@gmail.com>
2016-04-26 18:47:06 -0500

    Initial Commit

---
+++ StrategyPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StrategyPattern
+//
+//  Created by Md Adit Hasan on 4/26/16.
+//  Copyright © 2016 Md Adit Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
